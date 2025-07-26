075094221de5592a7d1fd1a1f9b6461ea54d9d70
Maxzolythus <bludoktor@gmail.com>
2025-07-25 22:12:03 -0500

    Fleshing out the skeleton

--- src/routes/track_stats.go
+++ src/routes/track_stats.go
@@ -1,30 +1,37 @@
 package routes
 
 import (
+	"context"
 	"encoding/json"
 	"log/slog"
 	"main/src/types"
 	"main/src/utils"
 	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 // TrackStatsHandler adds provided data to the DB
 func TrackStatsHandler(w http.ResponseWriter, r *http.Request) {
-	// get the values from the query
+	// get the values from the body
+	var stat types.DataPoint
+	err := json.NewDecoder(r.Body).Decode(&stat)
+	if err != nil {
+		slog.Error("TrackStats Error: " + err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"message": "TrackStats Error: " + err.Error()})
+	}
 
-	// escape string
-
-	stat := utils.FormatStats()
+	stat = utils.FormatStats(stat)
 
 	// access mongo
 	mongoClient := utils.SetupMongoClient(nil)
 
-	err := trackStats(mongoClient, stat)
+	err = trackStats(mongoClient, stat)
 	if err != nil {
 		slog.Error("TrackStats Error: " + err.Error())
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(map[string]string{"message": "TrackStats Error: " + err.Error()})
 	}
 
@@ -33,6 +40,18 @@ func TrackStatsHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"message": "Sucessfully added Stats"})
 }
 
-func trackStats(mongo *mongo.Client, stat types.DataPoint) error {
+func trackStats(mongoClient *mongo.Client, stat types.DataPoint) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	collection := mongoClient.Database("testing").Collection("numbers")
+
+	res, err := collection.InsertOne(ctx, stat)
+	if err != nil {
+		return err
+	}
+	id := res.InsertedID
+
+	slog.Info("Successfully inserted record: %s", id)
+
 	return nil
 }
