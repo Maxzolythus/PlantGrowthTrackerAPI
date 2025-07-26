a4075c2d95d8d5ca31590870852ddcca4b40d4b1
Maxzolythus <bludoktor@gmail.com>
2025-07-25 22:12:35 -0500

    Fleshing out tests

--- src/routes/track_stats_test.go
+++ src/routes/track_stats_test.go
@@ -1,21 +1,67 @@
 package routes
 
-// Test Happy Path 1 - All things filled out
+import (
+	"main/src/types"
+	"testing"
 
-// Test Happy Path 2 - Only Height
+	"github.com/stretchr/testify/assert"
+)
 
-// Test Happy Path 3 - Only Soil Moisture
+func TestTrackStats(t *testing.T) {
+	// Test Happy Path 1 - All things filled out
+	t.Run("TestTrackStatsAll", func(t *testing.T) {
+		// TODO Mongo Mock asserts
+		err := trackStats(nil, types.DataPoint{})
+		assert.NoError(t, err)
+	})
 
-// Test Happy Path 4 - Only Fertilizer
+	// Test Happy Path 2 - Only Height
+	t.Run("TestTrackStatsOnlyHeight", func(t *testing.T) {
 
-// Test Happy Path 5 - Only Temp
+	})
 
-// Test Happy Path - Only ph
+	// Test Happy Path 3 - Only Soil Moisture
+	t.Run("TestTrackStatsOnlySoilMoisture", func(t *testing.T) {
 
-// Test Sad Path 1 - Wrong Type
+	})
 
-// Test Sad Path 2 - Only time stamp
+	// Test Happy Path 4 - Only Fertilizer
+	t.Run("TestTrackStatsOnlyFetilizer", func(t *testing.T) {
 
-// Test Sad Path 3 - All nil
+	})
 
-// Test Route
+	// Test Happy Path 5 - Only Temp
+	t.Run("TestTrackStatsOnlyTemp", func(t *testing.T) {
+
+	})
+
+	// Test Happy Path - Only ph
+	t.Run("TestTrackStatsOnlyPH", func(t *testing.T) {
+
+	})
+
+	// Test Sad Path 1 - Wrong Type
+	t.Run("TestTrackStatsWrongType", func(t *testing.T) {
+
+	})
+
+	// Test Sad Path 2 - Only time stamp
+	t.Run("TestTrackStatsOnlyTimeStamp", func(t *testing.T) {
+
+	})
+
+	// Test Sad Path 3 - All nil
+	t.Run("TestTrackStatsAllNil", func(t *testing.T) {
+
+	})
+
+	// Test Sad Path 4 - Maliscious Text
+	t.Run("TestTrackStatsAllNil", func(t *testing.T) {
+
+	})
+
+	// Test Route
+	t.Run("TestTrackStatsRoute", func(t *testing.T) {
+
+	})
+}
