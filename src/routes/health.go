0f745a34269ca1eb06b4042a39aa86e44e30bccf
Maxzolythus <bludoktor@gmail.com>
2025-07-25 19:34:28 -0500

    Adding mongo check to health

--- src/routes/health.go
+++ src/routes/health.go
@@ -1,15 +1,28 @@
 package routes
 
 import (
+	"context"
 	"encoding/json"
+	"main/src/utils"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 // HealthHandler displays the health of the application and its dependancies
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	ctx := context.Background()
+	statusCode := http.StatusOK
+	mongoHealth := true
+	mongoClient := utils.SetupMongoClient(nil)
 
-	// TODO: Check the status of our DB and include them in the response.
-	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	err := mongoClient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		statusCode = http.StatusInternalServerError
+		mongoHealth = false
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(map[string]bool{"mongoHealth": mongoHealth})
 }
