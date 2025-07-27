f65490fee631af11ae32515191f34d6b40b5a9a7
Maxzolythus <bludoktor@gmail.com>
2025-07-27 17:37:26 -0500

    Adding docs

--- src/utils/mongo_client.go
+++ src/utils/mongo_client.go
@@ -7,11 +7,13 @@ import (
 	"time"
 )
 
+// MongoClient is the interface for Mongo actions that need to be performed
 type MongoClient interface {
 	InsertStat(stat types.DataPoint) error
 	GetStats(query string) ([]types.DataPoint, error)
 }
 
+// InsertStat inserts a single stat into the stats database
 func (mongo *Mongo) InsertStat(stat types.DataPoint) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -27,6 +29,7 @@ func (mongo *Mongo) InsertStat(stat types.DataPoint) error {
 	return nil
 }
 
+// GetStats returns all stats that match a given query
 func (mongo *Mongo) GetStats(query string) ([]types.DataPoint, error) {
 	return []types.DataPoint{}, nil
 }
