c5feee8c68b2538b1ac16337542242f4a309bb1a
Maxzolythus <bludoktor@gmail.com>
2025-11-12 17:17:05 -0600

    Improving valdation

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module main
 go 1.24.5
 
 require (
+	github.com/go-playground/validator/v10 v10.27.0
 	github.com/gorilla/mux v1.8.1
 	github.com/stretchr/testify v1.10.0
 	go.mongodb.org/mongo-driver/v2 v2.2.2
@@ -13,7 +14,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.27.0 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- src/types/data_points.go
+++ src/types/data_points.go
@@ -1,14 +1,12 @@
 package types
 
-import "time"
-
 // DataPoint is a representation of the data captured by our peripherals and stored in the DB
 type DataPoint struct {
-	Id           int       `json:"_id,omitempty" validate:"omitempty,mongodb"`
-	Timestamp    time.Time `json:"timestamp" validate:"required"`
-	Height       float64   `json:"height"`
-	SoilMoisture int       `json:"soilMoisture"`
-	PH           float64   `json:"ph" validate:"gte=0,lte=14"`
-	Fertilizer   float64   `json:"fertilizer"`
-	Tempurature  float64   `json:"tempurature"`
+	Id           int      `json:"_id,omitempty" validate:"omitempty,mongodb"`
+	Timestamp    string   `json:"timestamp" validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
+	Height       *float64 `json:"height,omitempty" validate:"omitempty,gte=0"`
+	SoilMoisture *int     `json:"soilMoisture,omitempty" validate:"omitempty,gte=0"`
+	PH           *float64 `json:"pH,omitempty" validate:"omitempty,gte=0,lte=14"`
+	Fertilizer   *float64 `json:"fertilizer,omitempty" validate:"omitempty,gte=0"`
+	Tempurature  *float64 `json:"tempurature,omitempty" validate:"omitempty"`
 }
