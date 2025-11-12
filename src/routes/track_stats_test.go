09b4b3711d68c69c599b442fb05f094286be53d4
Maxzolythus <bludoktor@gmail.com>
2025-11-12 17:17:31 -0600

    Fleshing out tests

--- src/routes/track_stats_test.go
+++ src/routes/track_stats_test.go
@@ -1,25 +1,52 @@
 package routes
 
 import (
+	"bytes"
+	"errors"
+	"io"
+	"log"
 	mockUtils "main/mocks/utils"
 	"main/src/types"
 	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestTrackStats(t *testing.T) {
-	// Test Happy Path 1 - All things filled out
+	height := 12.7
+	sm := 57
+	ph := 4.7
+	fert := 125.0
+	temp := 87.0
+	fullyFilledData := types.DataPoint{
+		Timestamp:    time.Now().Format("2006-01-02T15:04:05Z07:00"),
+		Height:       &height,
+		SoilMoisture: &sm,
+		PH:           &ph,
+		Fertilizer:   &fert,
+		Tempurature:  &temp,
+	}
+
+	// Test Happy Path - All things filled out
 	t.Run("TestTrackStatsAll", func(t *testing.T) {
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		mockMongo.EXPECT().InsertStat(fullyFilledData).Return(nil)
+		statusCode, _, err := trackStats(mockMongo, fullyFilledData)
+		assert.Equal(t, http.StatusOK, statusCode)
+		assert.NoError(t, err)
+	})
+
+	// Test Happy Path - Only Height
+	t.Run("TestTrackStatsOnlyHeight", func(t *testing.T) {
 		data := types.DataPoint{
-			Timestamp:    time.Now(),
-			Height:       12.7,
-			SoilMoisture: 57,
-			PH:           4.7,
-			Fertilizer:   125,
-			Tempurature:  87,
+			Timestamp: time.Now().Format("2006-01-02T15:04:05Z07:00"),
+			Height:    &height,
 		}
 		mockMongo := mockUtils.NewMockMongoClient(t)
 		mockMongo.EXPECT().InsertStat(data).Return(nil)
@@ -28,68 +55,265 @@ func TestTrackStats(t *testing.T) {
 		assert.NoError(t, err)
 	})
 
-	// Test Happy Path 2 - Only Height
-	t.Run("TestTrackStatsOnlyHeight", func(t *testing.T) {
-
-	})
-
-	// Test Happy Path 3 - Only Soil Moisture
+	// Test Happy Path - Only Soil Moisture
 	t.Run("TestTrackStatsOnlySoilMoisture", func(t *testing.T) {
-
+		data := types.DataPoint{
+			Timestamp:    time.Now().Format("2006-01-02T15:04:05Z07:00"),
+			SoilMoisture: &sm,
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		mockMongo.EXPECT().InsertStat(data).Return(nil)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusOK, statusCode)
+		assert.NoError(t, err)
 	})
 
-	// Test Happy Path 4 - Only Fertilizer
+	// Test Happy Path - Only Fertilizer
 	t.Run("TestTrackStatsOnlyFetilizer", func(t *testing.T) {
-
-	})
-
-	// Test Happy Path 5 - Only Temp
-	t.Run("TestTrackStatsOnlyTemp", func(t *testing.T) {
-
+		data := types.DataPoint{
+			Timestamp:  time.Now().Format("2006-01-02T15:04:05Z07:00"),
+			Fertilizer: &fert,
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		mockMongo.EXPECT().InsertStat(data).Return(nil)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusOK, statusCode)
+		assert.NoError(t, err)
 	})
 
 	// Test Happy Path - Only ph
 	t.Run("TestTrackStatsOnlyPH", func(t *testing.T) {
-
+		data := types.DataPoint{
+			Timestamp: time.Now().Format("2006-01-02T15:04:05Z07:00"),
+			PH:        &ph,
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		mockMongo.EXPECT().InsertStat(data).Return(nil)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusOK, statusCode)
+		assert.NoError(t, err)
 	})
 
-	// Test Sad Path 1 - Wrong Type
-	t.Run("TestTrackStatsWrongType", func(t *testing.T) {
-
+	// Test Happy Path - Only Temp
+	t.Run("TestTrackStatsOnlyTemp", func(t *testing.T) {
+		data := types.DataPoint{
+			Timestamp:   time.Now().Format("2006-01-02T15:04:05Z07:00"),
+			Tempurature: &temp,
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		mockMongo.EXPECT().InsertStat(data).Return(nil)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusOK, statusCode)
+		assert.NoError(t, err)
 	})
 
-	// Test Sad Path 2 - Only time stamp
+	// Test Happy Path - Mixed Nil Stats
+	t.Run("TestTrackStatsMixedNil", func(t *testing.T) {
+		data := types.DataPoint{
+			Timestamp:    time.Now().Format("2006-01-02T15:04:05Z07:00"),
+			Height:       &height,
+			SoilMoisture: &sm,
+			PH:           nil,
+			Fertilizer:   &fert,
+			Tempurature:  &temp,
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusOK, statusCode)
+		assert.NoError(t, err)
+	})
+
+	// Test Sad Path 2 - Only timestamp
 	t.Run("TestTrackStatsOnlyTimeStamp", func(t *testing.T) {
-
+		data := types.DataPoint{
+			Timestamp: time.Now().Format("2006-01-02T15:04:05Z07:00"),
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusBadRequest, statusCode)
+		assert.Error(t, err)
 	})
 
-	// Test Sad Path 3 - No time stamp
+	// Test Sad Path - No time stamp
 	t.Run("TestTrackStatsNoTimeStamp", func(t *testing.T) {
-
+		data := types.DataPoint{
+			Height:       &height,
+			SoilMoisture: &sm,
+			PH:           &ph,
+			Fertilizer:   &fert,
+			Tempurature:  &temp,
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusBadRequest, statusCode)
+		assert.Error(t, err)
 	})
 
-	// Test Sad Path 3 - Invalid time stamp
-	t.Run("TestTrackStatsInvalidTimeStamp", func(t *testing.T) {
-
-	})
-
-	// Test Sad Path 2 - Invalid pH
+	// Test Sad Path - Invalid pH
 	t.Run("TestTrackStatsInvalidPH", func(t *testing.T) {
-
+		invalidPh := 1400.0
+		data := types.DataPoint{
+			Timestamp:    time.Now().Format("2006-01-02T15:04:05Z07:00"),
+			Height:       &height,
+			SoilMoisture: &sm,
+			PH:           &invalidPh,
+			Fertilizer:   &fert,
+			Tempurature:  &temp,
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusBadRequest, statusCode)
+		assert.Error(t, err)
 	})
 
-	// Test Sad Path 3 - All nil
+	// Test Sad Path - Empty Struct
+	t.Run("TestTrackStatsEmpty", func(t *testing.T) {
+		data := types.DataPoint{}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusBadRequest, statusCode)
+		assert.Error(t, err)
+	})
+
+	// Test Sad Path - DB Error
+	t.Run("TestTrackStatsDBError", func(t *testing.T) {
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		mockMongo.EXPECT().InsertStat(fullyFilledData).Return(errors.New("Mongo Error"))
+		statusCode, _, err := trackStats(mockMongo, fullyFilledData)
+		assert.Equal(t, http.StatusInternalServerError, statusCode)
+		assert.Error(t, err)
+	})
+
+	// Test Sad Path - All Nil Stats
 	t.Run("TestTrackStatsAllNil", func(t *testing.T) {
+		data := types.DataPoint{
+			Timestamp:    time.Now().Format("2006-01-02T15:04:05Z07:00"),
+			Height:       nil,
+			SoilMoisture: nil,
+			PH:           nil,
+			Fertilizer:   nil,
+			Tempurature:  nil,
+		}
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		statusCode, _, err := trackStats(mockMongo, data)
+		assert.Equal(t, http.StatusBadRequest, statusCode)
+		assert.Error(t, err)
 
 	})
 
-	// Test Sad Path 4 - Maliscious Text
-	t.Run("TestTrackStatsAllNil", func(t *testing.T) {
+	// Test Route - Valid All
+	t.Run("TestTrackStatsRouteValid", func(t *testing.T) {
+		//The response recorder used to record HTTP responses
+		respRec := httptest.NewRecorder()
+		path := filepath.Join("..", "..", "test", "mock", "validTrackStatsBody.json")
 
+		body, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer body.Close()
+
+		req, err := http.NewRequest("POST", "/stats", body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		mockMongo.EXPECT().InsertStat(mock.Anything).Return(nil) // TODO: Test Value
+		router := SetupRouter(nil, mockMongo)
+
+		router.ServeHTTP(respRec, req)
+
+		assert.Equal(t, http.StatusOK, respRec.Code)
 	})
 
-	// Test Route
-	t.Run("TestTrackStatsRoute", func(t *testing.T) {
+	// Test Route - Optional Valid
+	t.Run("TestTrackStatsRouteOptionalValid", func(t *testing.T) {
+		//The response recorder used to record HTTP responses
+		respRec := httptest.NewRecorder()
 
+		path := filepath.Join("..", "..", "test", "mock", "validTrackStatsOptionalBody.json")
+
+		body, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer body.Close()
+
+		req, err := http.NewRequest("POST", "/stats", body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		mockMongo.EXPECT().InsertStat(mock.Anything).Return(nil) // TODO: Test Value
+		router := SetupRouter(nil, mockMongo)
+
+		router.ServeHTTP(respRec, req)
+
+		assert.Equal(t, http.StatusOK, respRec.Code)
+	})
+
+	// Test Route - Invalid Empty Body
+	t.Run("TestTrackStatsRouteEmptyBody", func(t *testing.T) {
+		//The response recorder used to record HTTP responses
+		respRec := httptest.NewRecorder()
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		router := SetupRouter(nil, mockMongo)
+
+		emptyJSONBody := io.NopCloser(bytes.NewReader([]byte("{}")))
+
+		req, err := http.NewRequest("POST", "/stats", emptyJSONBody)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		router.ServeHTTP(respRec, req)
+
+		assert.Equal(t, http.StatusBadRequest, respRec.Code)
+	})
+
+	// Test Route - Invalid Body
+	t.Run("TestTrackStatsRouteInvalidBody", func(t *testing.T) {
+		//The response recorder used to record HTTP responses
+		respRec := httptest.NewRecorder()
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		router := SetupRouter(nil, mockMongo)
+
+		path := filepath.Join("..", "..", "test", "mock", "invalidTrackStatsBody.json")
+
+		body, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer body.Close()
+
+		req, err := http.NewRequest("POST", "/stats", body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		router.ServeHTTP(respRec, req)
+
+		assert.Equal(t, http.StatusBadRequest, respRec.Code)
+	})
+
+	// Test Route 3 - Invalid Nil Body
+	t.Run("TestTrackStatsRouteNilBody", func(t *testing.T) {
+		//The response recorder used to record HTTP responses
+		respRec := httptest.NewRecorder()
+		mockMongo := mockUtils.NewMockMongoClient(t)
+		router := SetupRouter(nil, mockMongo)
+
+		nilJSONBody := io.NopCloser(bytes.NewReader(nil))
+
+		req, err := http.NewRequest("POST", "/stats", nilJSONBody)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		router.ServeHTTP(respRec, req)
+
+		assert.Equal(t, http.StatusBadRequest, respRec.Code)
 	})
 }
